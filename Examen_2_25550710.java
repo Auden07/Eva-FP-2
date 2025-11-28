fa129cfb5c81b5cc24800c0197362931b4df88bf
Auden07 <L25550710@chihuahua2.tecnm.mx>
2025-11-27 19:28:26 -0600

    Add files via upload

---
+++ Examen_2_25550710.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package examen_2_25550710;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class Examen_2_25550710 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner (System.in);
+         int opcn = 0,opcn2;
+        System.out.println("ingrese un numero 1 o 2 para solicitar numeros primos oh no primos");
+        System.out.println("1.numero NO PRIMOS)");
+        System.out.println("2.numero PRIMOS");
+        
+        System.out.println("imprimir los numero primos del (2 al 8)");
+        opcn2 = captu.nextInt();    
+        
+        System.out.println("imprimir los numeros no primos del (2 al 8)");
+        opcn = captu.nextInt();
+        
+         if (opcn2 <= 2) {
+            System.out.println("2**, 3***, 5*****, 7*******");
+        if (opcn <= 1){
+            System.out.println("4****, 6******, 8********");
+            
+                boolean usuario = false;
+                
+        if (usuario) {
+            System.out.println("Usted determine como hacerlo");
+            
+            
+        }    
+        }else{
+            
+       
+        }
+         }
+    }
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+
